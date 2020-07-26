2823a01c96050edb02ea2bdc36515dd914d132bb
MilanBojic1999 <mbojic12@gmail.com>
2020-07-26 18:30:42 +0200

    added new class for functions

--- model/parser.py
+++ model/parser.py
@@ -11,6 +11,10 @@ __Constants = {
 }
 
 
+def iden(x):
+    return x
+
+
 def mypow(x, p):
     return x ** p
 
@@ -27,7 +31,18 @@ def imag(x):
     return x
 
 
+__Variables = ('x', 'y', 'z', 't')
+
+__Operation = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2,
+
+}
+
 __Functions = {
+    'x': iden,
     'real': real,
     'imag': imag,
     'abs': abs,
@@ -41,12 +56,49 @@ __Functions = {
 }
 
 
+class StackFunction:
+    def __init__(self, f, sf=None):
+        self.f = f
+        self.sf = None
+        self.sf = sf
+
+    def evaluate(self, num):
+        if self.sf is not None:
+            x = self.sf.evaluate(num)
+            return self.f(x)
+
+        return self.f(num)
+
+
+def basic_parser(s: str) -> StackFunction:
+    vals = []
+    stck = []
+
+
+def fun_parser(s: str) -> StackFunction:
+    if str[0] in __Variables:
+        return StackFunction(iden)
+    f: StackFunction
+
+    index = s.find('(')
+    if not (index not in (3, 4)):
+        raise Exception('Not a function to parse')
+
+    temp = s[0:index]
+    print(temp)
+
+    if temp in __Functions:
+        f = __Functions.get(temp)
+    else:
+        raise Exception('Not a function to parse')
+
+    end = s[::-1].find(')')
+
+    return f(str[index + 1:end])
+
+
 class Parser:
     def __init__(self, expr: str):
         self.expr = expr
-
-    def basic_parser(self, s: str):
-        pass
-
-    def fun_parser(self, s: str):
-        pass
+        self.fun = basic_parser(expr)
+        self.val = 0
