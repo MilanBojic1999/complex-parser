d2bfe770c4cec93b1a7eab3f599ecb02ff7b705b
MilanBojic1999 <mbojic12@gmail.com>
2020-07-27 12:19:59 +0200

    Now you can plot function on real plane (red color) and complex plane(blue color)

--- graphicon.py
+++ graphicon.py
@@ -7,23 +7,47 @@ def null_line(x):
     return 0
 
 
-fun = input('Put your function: ')
+def complex_plot(expressions: str):
+    arr = np.arange(-5., 5., 0.02, dtype='complex_')
 
-arr = np.arange(-5., 5., 0.02, dtype='complex_')
+    p = Parser(expressions)
 
-p = Parser(fun)
+    f1 = np.vectorize(p.real_exec, otypes=[complex])
+    f2 = np.vectorize(p.imag_exec, otypes=[complex])
+    nf = np.vectorize(null_line, otypes=[complex])
 
-f1 = np.vectorize(p.real_exec, otypes=[complex])
-f2 = np.vectorize(p.imag_exec, otypes=[complex])
-nf = np.vectorize(null_line, otypes=[complex])
+    fig, ax = pl.subplots()
 
-fig, ax = pl.subplots()
+    ax.plot(arr, nf(arr), color='green')
+    ax.plot(nf(arr), arr, color='green')
+    ax.plot(f1(arr), f2(arr), color='blue')
 
-ax.plot(arr, nf(arr), color='green')
-ax.plot(nf(arr), arr, color='green')
-# ax.plot(f1(arr), f2(arr), color='blue')
-ax.plot(arr, f1(arr), color='red')
+    pl.grid(True)
 
-pl.grid(True)
+    pl.show()
 
-pl.show()
+
+def fun_plot(expressions: str):
+    arr = np.arange(-5., 5., 0.02, dtype='complex_')
+
+    p = Parser(expressions)
+
+    f1 = np.vectorize(p.real_exec, otypes=[complex])
+    f2 = np.vectorize(p.imag_exec, otypes=[complex])
+    nf = np.vectorize(null_line, otypes=[complex])
+
+    fig, ax = pl.subplots()
+
+    ax.plot(arr, nf(arr), color='green')
+    ax.plot(nf(arr), arr, color='green')
+    ax.plot(arr, f1(arr), color='red')
+
+    pl.grid(True)
+
+    pl.show()
+
+
+fun = input("Input yout function: ")
+
+fun_plot(fun)
+complex_plot(fun)
