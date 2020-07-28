e9b0bc2f33ca406c1f398561b67e74c7ed774644
MilanBojic1999 <mbojic12@gmail.com>
2020-07-29 00:51:56 +0200

    GUI part of Application (Kivy library is used for this purpose)

--- view/app.py
+++ view/app.py
@@ -1,12 +1,29 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty
+
+
+class AppView(Widget):
+    tinp = ObjectProperty(None)
+
+    def funs(self):
+        print('Kliknu sam ovo')
+
+    def prt(self,inst,val):
+        print('This sedngs:',inst,'and its a',val)
 
 
 class ComplexApp(App):
 
+
+
     def build(self):
-        return Label(text='Pozdrav')
+        v = AppView()
+        return v
 
 
 app = ComplexApp().run()
+
+print(app)
